af52f940f6bea68d08b193c2c5732ae0fc693bd9
Zamanian <zamanian@alay.ir>
2023-10-02 16:03:32 +0330

    one

--- src/component/App.js
+++ src/component/App.js
@@ -1,14 +1,12 @@
-import react, {useState} from 'react'
 import  Navbar from './Navbar'
 const App = () => {
     return (
         <div>
             <Navbar/>
-            <img  className=" bg-cover  z-0 rounded-xl" src={require("../images/hero_2.jpg")}/>
+            {/*<img  className=" bg-cover  z-0 rounded-xl" src={require("../images/hero_2.jpg")}/>*/}
+            <div style={{backgroundImage:"/image/hero_2.jpg"}}></div>
 
         </div>
-
-
 
     )
 }
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,16 +1,23 @@
-import react from "react";
 const Navbar = () => {
-   return(
-       <div className="w-3/4 bg-gray-100 h-24 flex items-center  rounded-xl p-8 ml-36 mt-16">
-           <h1><span className=" text-2xl">CLASS</span><span className="text-emerald-400 text-2xl">YADS</span></h1>
-           <ul className=" flex items-center ml-48 ">
-               <li className="m-4  hover:text-emerald-400">Home</li>
-               <li className="m-4 hover:text-emerald-400 ">Ads</li>
-               <li className="m-4  hover:text-emerald-400">About</li>
-               <li className="m-4 hover:text-emerald-400 ">Blog</li>
-               <li className="m-4 hover:text-emerald-400 ">Contact</li>
-           </ul>
-       </div>
-   )
+    return (
+        <div className="w-3/4 bg-gray-100 h-20 flex items-center  rounded-xl p-8 ml-36 mt-16">
+            <h1><span className=" text-2xl">CLASS</span><span className="text-emerald-400 text-2xl">YADS</span></h1>
+            <ul className=" flex items-center ml-48 ">
+                <li className="m-4  hover:text-emerald-400">Home</li>
+                <li className="m-4 hover:text-emerald-400 ">Ads</li>
+                <li className="m-4  hover:text-emerald-400">About</li>
+                <li className="m-4 hover:text-emerald-400 ">Blog</li>
+                <li className="m-4 hover:text-emerald-400 ">Contact</li>
+            </ul>
+
+            <ul className=" flex items-center ml-12 mr-4 ">
+
+                <li className="m-4   hover:text-emerald-400">Login</li>
+
+                <li className="m-4  hover:text-emerald-400">Register</li>
+            </ul>
+            <button className=" w-32 h-10 rounded-xl bg-emerald-300 ml-2 text-white">+Post an Ad</button>
+        </div>
+    )
 }
 export default Navbar
