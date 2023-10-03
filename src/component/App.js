cfd068a54b2cb23a94b2c2553ed8d05b0cfde100
arezoo <arezoo.rahbari78@gmail.com>
2023-10-04 00:15:10 +0330

    one

--- src/component/App.js
+++ src/component/App.js
@@ -1,11 +1,35 @@
-import  Navbar from './Navbar'
+import Navbar from './Navbar'
+
 const App = () => {
     return (
         <div>
-            <Navbar/>
-            {/*<img  className=" bg-cover  z-0 rounded-xl" src={require("../images/hero_2.jpg")}/>*/}
-            <div style={{backgroundImage:"/image/hero_2.jpg"}}></div>
+            <div>
+                <div className=" p-16 h-[1000px]  w-[full]   bg-[url('./image/hero_2.jpg')] mix-blend-overlay ">
+                    <Navbar/>
+                    <div className="text-center mt-36 ">
+                        <h1 className="text-5xl text-white  ">Largest Classifieds In The World</h1>
+                        <h2 className="text-xl mt-8 ">You can buy, sell anything you want.
+                        </h2>
+                    </div>
+                    <div
+                        className=" flex justify-items-center rounded-xl ml-40 text-center mt-36 w-3/4 h-46 bg-white opacity-70	">
+                        <input placeholder="What are you looking for? " type="text"
+                               className="border  rounded m-4 h-8 w-56 p-2 opacity-100"/>
+                        <input placeholder="Location " type="text"
+                               className="border  rounded m-4 h-8 w-56 p-2 opacity-100"/>
+                        {/*<input type="text" className="border rounded m-4 h-8 w-56 p-2 " />*/}
+                        <select className="border rounded m-4 h-8 w-56 pl-2 opacity-100">
+                            <option className="selected">All categories</option>
+                            <option>All categories</option>
+                            <option>All categories</option>
+                            <option>All categories</option>
+                        </select>
+                        <button className="w-36 h-8 rounded bg-emerald-300 m-4"> Serch</button>
+                    </div>
+                    <div classname="w-"></div>
 
+                </div>
+            </div>
         </div>
 
     )
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,6 +1,6 @@
 const Navbar = () => {
     return (
-        <div className="w-3/4 bg-gray-100 h-20 flex items-center  rounded-xl p-8 ml-36 mt-16">
+        <div className="w-3/4 bg-white h-20 flex items-center  rounded-xl p-8 ml-36  ">
             <h1><span className=" text-2xl">CLASS</span><span className="text-emerald-400 text-2xl">YADS</span></h1>
             <ul className=" flex items-center ml-48 ">
                 <li className="m-4  hover:text-emerald-400">Home</li>
